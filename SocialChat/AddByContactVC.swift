153a11b3944a984e2505e0897a4db53a8d2444ba
angush <angush@student.unimelb.edu.au>
2016-10-09 16:52:03 +1100

    no message

---
+++ SocialChat/AddByContactVC.swift
@@ -0,0 +1,39 @@
+//
+//  AddByContactVC.swift
+//  SocialChat
+//
+//  Created by zheng chai on 9/10/2016.
+//  Copyright © 2016 Social Media Coders. All rights reserved.
+//
+
+import UIKit
+
+class AddByContactVC: UIViewController {
+
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
