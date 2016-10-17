08e0760ce4aa6e511ef10c334e079ca8dc1c0a76
angush <angush@student.unimelb.edu.au>
2016-10-18 01:23:36 +1100

    no message

--- SocialChat/FaceDetect.swift
+++ SocialChat/FaceDetect.swift
@@ -14,24 +14,40 @@ class FaceDetect: UIViewController {
     @IBOutlet var myImage: UIImageView!
     
     var newImage: UIImage?
+    var faceExpress = "the faces are"
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
         myImage.image = newImage
+//        myImage.image = #imageLiteral(resourceName: "face")
+       findFace()
     }
 
     func findFace(){
 //        guard let faceImage = CIImage(cgImage: newImage!.cgImage!) else{return}
         let faceImage = CIImage(cgImage: (newImage?.cgImage)!)
-        
+        //let faceImage = CIImage(cgImage: #imageLiteral(resourceName: "face").cgImage!)
         let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
         let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
-        let faces = faceDetector?.features(in: faceImage)
+       // let faces = faceDetector?.features(in: faceImage)
+        let faces = faceDetector?.features(in: faceImage, options: [CIDetectorSmile: true, CIDetectorEyeBlink: true])
         for face in faces as! [CIFaceFeature]{
             if face.hasSmile{
-                
+                label.text = faceExpress.appending(" smile ")
             }
+            
+            if face.leftEyeClosed{
+                label.text = faceExpress.appending(" left eyes closed")
+            }
+            
+            if face.rightEyeClosed{
+                label.text = faceExpress.appending(" right eyes closed")
+            }
+        }
+        
+        if faces?.count == 0{
+            label.text = "no faces"
         }
         
     }
