b70c652875411b4b579084c621a3766442c76397
angush <angush@student.unimelb.edu.au>
2016-10-17 18:24:41 +1100

    no message

---
+++ SocialChat/JSQVisibleMediaItem.h
@@ -0,0 +1,13 @@
+//
+//  JSQVisibleMediaItem.h
+//  SocialChat
+//
+//  Created by zheng chai on 17/10/2016.
+//  Copyright © 2016 Social Media Coders. All rights reserved.
+//
+
+#ifndef JSQVisibleMediaItem_h
+#define JSQVisibleMediaItem_h
+
+
+#endif /* JSQVisibleMediaItem_h */
